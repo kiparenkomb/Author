60583eac16e1d994b3a38113ba60a23237428e57
kiparenkomb <kiparenkomb@gmail.com>
2017-02-10 07:44:03 +0200

    Author v_1.2.1

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,15 @@
 import HP from './helpers';
+import './my-owl-carousel';
+import './tilt';
+
 
 (function ($) {
 	$(document).ready(function(){
 
-		// menu click event
+/*------------------------------------------------------------------*\
+													Mobile menu
+\*------------------------------------------------------------------*/
+		//menu click event
 		$('.menuBtn').click(function() {
 			$(this).toggleClass('act');
 				if($(this).hasClass('act')) {
@@ -14,14 +20,56 @@ import HP from './helpers';
 				}
 		});
 
-		// banner
-		$(".owl-carousel").owlCarousel({
-			items: 1,
-			autoplay: true,
-			nav: true,
-			dots: false,
-			// animateOut: 'fadeOut'
+/*------------------------------------------------------------------*\
+													Footer
+\*------------------------------------------------------------------*/
+		//prallax scroll
+		//init controller
+		var controller = new ScrollMagic.Controller();
+		//loop through each .bcg_parallax element
+		$('.bcg_parallax').each(function(){
+			// if (document.documentElement.clientWidth > 425) {
+				//parallax scene
+				var paralaxTl = new TimelineMax();
+				paralaxTl
+					.from('.bcg', 2, {y: '-75%', ease:Power0.easeNone}, 0);
+				var slideParallaxScene = new ScrollMagic.Scene({
+					triggerElement: this,
+					triggerHook: 1,
+					duration: '100%'
+				})
+				.setTween(paralaxTl)
+				// .addIndicators({
+				// 		name: 'fade scene',
+				// 		color: 'red',
+				// 		indent: 100,
+				// 		colorStart: 'black',
+				// 		colorEnd: 'pink'
+				// 	})
+				.addTo(controller);
+			// }
 		});
+
+		//form validation
+		$(function() {
+			$("#form_validation").validate({
+				rules:{
+					text_name:{
+						required:true,
+						minlength:2
+					},
+					text_email:{
+						required:true,
+						email:true
+					},
+					text:{
+						required:true,
+						minlength:5,
+					}
+				}
+			});
+		});
+
 
 	});
 }(jQuery));
